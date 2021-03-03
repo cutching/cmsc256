1c3ac1440ca945cc31efec851ac4a2f09a53c46e
cutching <69918266+cutching@users.noreply.github.com>
2021-03-03 16:21:47 -0500

    Add files via upload

---
+++ cmsc256/MyIndexOutOfBoundsException.java
@@ -0,0 +1,12 @@
+package Project2.cmsc256;
+
+public class MyIndexOutOfBoundsException extends RuntimeException {
+
+	public MyIndexOutOfBoundsException(String message) {
+		super(message);
+	}
+
+	public MyIndexOutOfBoundsException() {
+		super();
+	}
+}
